6e4d09922ca4558645b7ae23481c5b82f9f54fcb
hs2323 <hs2323@gmail.com>
2026-05-21 07:43:13 -0400

    Updating assets path output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   build: {
+    assets: "assets",
     inlineStylesheets: "always",
   },
   integrations: [react()],
